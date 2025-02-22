6639b1ac30c03b58648d3d21173548b0c3d2d78c
Rohan Putcha <fastbiker703@gmail.com>
2025-02-22 12:34:40 -0500

    Initial Commit

---
+++ NotificationApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotificationApp
+//
+//  Created by Rohan Putcha on 2/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotificationApp/NotificationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationAppApp.swift
+//  NotificationApp
+//
+//  Created by Rohan Putcha on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct NotificationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
